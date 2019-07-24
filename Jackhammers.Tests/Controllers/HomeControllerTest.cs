05bafe3adfdef22ebb4938ce0c85298af033a1e6
enikolaenko <enikolaenko@upstreamworks.com>
2019-07-24 09:45:57 -0400

    Add initial projects

---
+++ Jackhammers.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Jackhammers.Controllers;
+
+namespace Jackhammers.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ Jackhammers/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Jackhammers.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
