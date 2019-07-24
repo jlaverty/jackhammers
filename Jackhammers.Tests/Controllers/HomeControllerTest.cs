f49671e9dba59c6913b76871cce0538b646bd871
enikolaenko <enikolaenko@upstreamworks.com>
2019-07-24 14:49:41 -0400

    Demo commit

--- Jackhammers.Tests/Controllers/HomeControllerTest.cs
+++ Jackhammers.Tests/Controllers/HomeControllerTest.cs
@@ -21,7 +21,7 @@ namespace Jackhammers.Tests.Controllers
         [Fact]
         public void Test1()
         {
-            Assert.True(false);
+            Assert.True(true);
         }
     }
 }
