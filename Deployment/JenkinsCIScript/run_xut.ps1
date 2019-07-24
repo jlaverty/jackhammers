0572507937703a336751589675cfac3d9fc9ed3d
enikolaenko <enikolaenko@upstreamworks.com>
2019-07-24 10:34:44 -0400

    Add run_xut.ps1

---
+++ Deployment/JenkinsCIScript/run_xut.ps1
@@ -0,0 +1,7 @@
+$testProjects = Get-ChildItem -Path $env:WORKSPACE -Filter "*.UnitTest.dll" -Recurse | ForEach-Object FullName | Select-String -Pattern "bin\Debug" -SimpleMatch
+$xconsole = [IO.Path]::Combine($env:WORKSPACE, "packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe")
+foreach ($project in $testProjects)
+{
+    $testXmlPath = [IO.Path]::Combine((Get-Item $project).DirectoryName, (Get-Item $project).Basename + ".xml")
+    cmd /c $xconsole $project -xml $testXmlPath
+}
