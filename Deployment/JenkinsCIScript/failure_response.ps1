5d62c50e57ed236635002e533697b4eedc4127bb
enikolaenko <enikolaenko@upstreamworks.com>
2019-07-24 13:00:05 -0400

    Add git response scripts

---
+++ Deployment/JenkinsCIScript/failure_response.ps1
@@ -0,0 +1,8 @@
+$uri = "https://api.GitHub.com/repos/jlaverty/jackhammers/statuses/{$env::GIT_COMMIT}?access_token=bd6c3b40780fcc93271ba6d8c69d305a9e472e7e";
+$body = @{
+"state" = "failure"
+"context" = "continuous-integration/jenkins"
+"description" = "Jenkins"
+"target_url" = "https://3d36289d.ngrok.io/job/ci_demo/{$env::BUILD_NUMBER}/console"
+}
+Invoke-RestMethod -Method 'Post' -Uri $uri -ContentType "application/json" -Body $body
